4ace8c0b09cb93a02ef6190acf9e8bfc7d755abc
Vamsi Chunduru <cvamsikrishna11@gmail.com>
2022-11-03 19:40:50 -0400

    fix for checkov 135,126

--- ec2.tf
+++ ec2.tf
@@ -1,6 +1,9 @@
 resource "aws_instance" "web" {
   ami           = var.AMI_ID
   instance_type = var.INSTANCE_TYPE
+  monitoring = true
+  ebs_optimized = true
+
 
   tags = {
     Name = "airbnb-web-server"
